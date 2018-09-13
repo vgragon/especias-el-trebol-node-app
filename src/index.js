5fe1935d9f90093a6ec887a8f6b4ad6a1278afdb
Vicente Grajeda <vicente.grajeda@base22.com>
2018-09-13 12:21:14 -0400

    Add CORS for all origins

--- src/index.js
+++ src/index.js
@@ -17,8 +17,10 @@ app.use(morgan('combined', {
     }
 }));
 
-app.get('/', function (req, res) {
-    res.send('Hello World');
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
 });
 
 for (let route in routes) {
