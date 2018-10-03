46574211809aa0c2f4e2cd89c7308491641e41da
Vicente Grajeda <vicente.grajeda@base22.com>
2018-10-03 16:01:57 -0500

    Add header to allow GET, POST, DELETE operations

--- src/app/index.js
+++ src/app/index.js
@@ -20,6 +20,7 @@ app.use(morgan('combined', {
 
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,POST,DELETE");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
