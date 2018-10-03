461e876fc506c3131eb789070e36ee263f2e8612
Vicente Grajeda <vicente.grajeda@base22.com>
2018-10-03 16:02:53 -0500

    Add read, create, update, delete logic for clients

--- src/app/clients.js
+++ src/app/clients.js
@@ -1,42 +1,54 @@
 let MongoUtil = require('./../db');
 const {ObjectID} = require('mongodb'); // or ObjectID
+const models = require('./../db/model');
 
 module.exports = {
-    read: function (query) {
-        return new Promise((resolve, reject) => {
-            let dbClient = MongoUtil.getDatabaseClient();
+    read: function (id, load = []) {
+        let queryParams = {};
+        if (typeof id !== "undefined") queryParams["_id"] = id;
 
-            dbClient.collection("clients").find(query).toArray(function (err, result) {
-                if (err) reject(err);
-                resolve(result);
-            });
+        let query = models.Client.find(queryParams);
+
+        return new Promise((resolve, reject) => {
+            query.exec()
+                .then(result => {
+                    resolve(result);
+                })
+                .catch(err => {
+                    console.error(err);
+                });
         });
     },
     create: function (clientObject) {
         return new Promise((resolve, reject) => {
-            let dbClient = MongoUtil.getDatabaseClient();
             let original_id = ObjectID();
 
-            let promise = dbClient.collection('clients').insertOne({
+            let promise = models.Client.create({
                 "_id": original_id,
                 ...clientObject
             });
 
             promise.then((result) => {
-                return dbClient.collection('clients').findOne({'_id': result.insertedId});
-            }).then((result) => {
                 resolve(result);
             }).catch(err => reject(err));
         });
     },
-    update: function (id, salesObject) {
+    update: function (clientObject) {
         return new Promise((resolve, reject) => {
+            let promise = models.Client.findOneAndUpdate({_id: clientObject["_id"]}, clientObject, {new: true});
 
+            promise.then((result) => {
+                resolve(result);
+            }).catch(err => reject(err));
         });
     },
     delete: function (id) {
         return new Promise((resolve, reject) => {
+            let promise = models.Client.findOneAndDelete({_id: id});
 
+            promise.then((result) => {
+                resolve(result);
+            }).catch(err => reject(err));
         });
     }
 };
--- src/routing/clients.js
+++ src/routing/clients.js
@@ -3,7 +3,11 @@ let router = express.Router();
 let clientsUtil = require('./../app/clients');
 
 router.get('/', function (req, res) {
-    clientsUtil.read({}).then((clients) => {
+    let {id, load} = req.query;
+    load = load || "";
+    load = load.split(',');
+
+    clientsUtil.read(id, load).then((clients) => {
         res.send(clients);
     });
 });
@@ -11,6 +15,25 @@ router.get('/', function (req, res) {
 router.post('/create', function (req, res) {
     let params = req.body;
     clientsUtil.create(params).then(function (_response) {
+        res.status(201).json(_response)
+    }).catch(function (err) {
+        res.status(400).json({error: err});
+    });
+});
+
+router.post('/update', function (req, res) {
+    let params = req.body;
+    clientsUtil.update(params).then(function (_response) {
+        res.status(201).json(_response)
+    }).catch(function (err) {
+        res.status(400).json({error: err});
+    });
+});
+
+router.delete('/delete', function (req, res) {
+    let params = req.body;
+    let {id} = params;
+    clientsUtil.delete(id).then(function (_response) {
         res.status(201).json(_response)
     }).catch(function (err) {
         res.status(400).json({error: err});
